17534dc1891d420b066233224b105cd7be801249
jithish <jithusekae@gmail.com>
2020-04-16 14:10:52 +0530

    hover

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -39,49 +39,59 @@ class Landingpage extends Component {
 
     render() {
         return (
-     <div>
-            <div style={{ width: "100%", margin: "auto" }} className="rel-inline">
-                <Grid className="landing-grid">
-                    <cell col={12} className='cell'>
-                        <h1 className="landh1">MediKare</h1>
-                        <p className="landp">("Pills & leaves!") </p>
-                        <hr />
-                        <p className="peng">It is treated in the form of a pill or a liquid.</p>
-                        <p>"Try English"</p>
-                        <button
-                            className='button'
-                            onClick={() => this.handleOnSubmit()}
-                        >English Medicine</button>
-                        <hr />
-                        <p className="payur">I got the bill for my surgery. Now I know what those doctors were wearing masks for.</p>
-                        <p>"Try Ayurvedic"</p>
-                        <button
-                            className='button'
-                            onClick={() => this.handleOnSubmit1()}
-                        >Ayurvedic Medicine</button><hr />
+            <div>
+                <div style={{ width: "100%", margin: "auto" }} className="rel-inline">
+                    <Grid className="landing-grid">
+                        <cell col={12} className='cell'>
+                            <h1 className="landh1">MediKare</h1>
+                            <p className="landp">("Pills & leaves!") </p>
+                            <hr />
+                            <p className="peng">It is treated in the form of a pill or a liquid.</p>
+                            <p>"Try English"</p>
+                            <button
+                                className='button'
+                                onClick={() => this.handleOnSubmit()}
+                            >English Medicine</button>
+                            <hr />
+                            <p className="payur">I got the bill for my surgery. Now I know what those doctors were wearing masks for.</p>
+                            <p>"Try Ayurvedic"</p>
+                            <button
+                                className='button'
+                                onClick={() => this.handleOnSubmit1()}
+                            >Ayurvedic Medicine</button><hr />
 
-                        <div className="fel">
-                            <h1 className='happy'>We are happy to serve you pills and leaves.</h1><br/>
+                            <div className="fel">
+                                <h1 className='happy'>We are happy to serve you pills and leaves.</h1><br />
                                 <button className="btn1"
-                                title ='find medicine in above english and ayurvedixc'
+                                    title='find medicine in above english and ayurvedic'
                                 >Womens Issues</button>
-                            <button className="btn1" >Genral Physician</button>
-                            <button className="btn1" >Hair and Scalp</button>
-                            <button className="btn1" >stress and Mental Health</button>
-                            <button className="btn1" >Skin Problems</button>
-                            <button className="btn1" >Pregnancy Problems</button>
+                                <button className="btn1"
+                                    title='find medicine in above english and ayurvedic'
+                                >Genral Physician</button>
+                                <button className="btn1"
+                                    title='find medicine in above english and ayurvedic'
+                                >Hair and Scalp</button>
+                                <button className="btn1"
+                                    title='find medicine in above english and ayurvedic'
+                                >stress and Mental Health</button>
+                                <button className="btn1"
+                                    title='find medicine in above english and ayurvedic'
+                                >Skin Problems</button>
+                                <button className="btn1"
+                                    title='find medicine in above english and ayurvedic'
+                                >Pregnancy Problems</button>
+                            </div>
+                            <br />
+                        </cell>
+                    </Grid>
+                    {this.state.showPopup === true &&
+                        <div>
+                            <Popup type={this.state.popupType} />
+                            <div onClick={e => { this.closePopup() }} className="overlay"></div>
                         </div>
-                        <br />
-                    </cell>
-                </Grid>
-                {this.state.showPopup === true &&
-                    <div>
-                        <Popup type={this.state.popupType} />
-                        <div onClick={e => { this.closePopup() }} className="overlay"></div>
-                    </div>
-                }
+                    }
+                </div>
             </div>
-</div>
         )
     }
 }
